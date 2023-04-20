84dc0e937e48530df5696490bb7161b74af0bf0b
cconway1-stevens <55499408+cconway1-stevens@users.noreply.github.com>
2023-04-19 22:48:44 -0400

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import PlausibleProvider from 'next-plausible'
-
+import
+ { Analytics } 
+from
+ 
+'@vercel/analytics/react'
+;
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
@@ -10,6 +15,7 @@ export default function App({
     <PlausibleProvider domain="demo.vercel.im">
       <SessionProvider session={session}>
         <Component {...pageProps} />
+        <Analytics/>
       </SessionProvider>
     </PlausibleProvider>
   )
